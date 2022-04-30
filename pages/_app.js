ce29dc7d55abc4d768fc33d31313a2c42c34e65c
federico_enriquez <federico.enriquez@mobbex.com>
2022-04-30 00:00:28 -0300

    adding improves

---
+++ pages/_app.js
@@ -0,0 +1,39 @@
+import { ThemeProvider, CSSReset } from '@chakra-ui/react';
+import { Global, css } from '@emotion/react';
+
+import { AuthProvider } from '@/lib/auth';
+import theme from '@/styles/theme';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  )
+}
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <CSSReset />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import { Button, Code, Heading, Text } from '@chakra-ui/react';
+import Head from 'next/head';
+import { useAuth } from '@/lib/auth';
+
+const Home = () => {
+  const auth = useAuth();
+
+  return (
+    <div className='container'>
+      <Head>
+        <title>Fast Feedback</title>
+      </Head>
+      <main>
+        <Heading>Fast Feedback</Heading>
+        <Text>
+          Current User: <Code>{auth?.user ? auth.user.email : 'None'}</Code>
+        </Text>
+        {auth.user ? (
+          <Button onClick={() => auth.signOut()}>Sign Out</Button>
+        ) : (
+          <Button onClick={() => auth.signinWithGitHub()}>Sign in</Button>
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default Home;
