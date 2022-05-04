6b2ad30d1f8bc2e07d2ddfaf57700cf4161c8578
federico_enriquez <federico.enriquez@mobbex.com>
2022-05-03 23:58:06 -0300

    chakra-ui added & initialized. Code improves. Also db, _document & theme files added

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,39 @@
-import { ProvideAuth } from '../lib/auth';
-import '../styles/globals.css';
+import { ThemeProvider, CSSReset } from '@chakra-ui/react';
+import { Global, css } from '@emotion/react';
 
-function MyApp({ Component, pageProps }) {
+import { AuthProvider } from '@/lib/auth';
+import theme from '@/styles/theme';
+
+const GlobalStyle = ({ children }) => {
   return (
-    <ProvideAuth>
-      <Component {...pageProps} />
-    </ProvideAuth>
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
   );
-}
+};
 
-export default MyApp;
+const App = ({ Component, pageProps }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
